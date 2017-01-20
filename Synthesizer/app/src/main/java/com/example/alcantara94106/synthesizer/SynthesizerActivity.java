3ed61e5845a8843c3e28d77b57d22d344d57b7fe
alcantara94106 <alcantara4106@mydusd.org>
2017-01-20 11:44:48 -0800

    up to #23 :smiley_cat:

--- Synthesizer/app/src/main/java/com/example/alcantara94106/synthesizer/SynthesizerActivity.java
+++ Synthesizer/app/src/main/java/com/example/alcantara94106/synthesizer/SynthesizerActivity.java
@@ -2,11 +2,17 @@ package com.example.alcantara94106.synthesizer;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class SynthesizerActivity extends AppCompatActivity {
-
+    private static final String TAG = SynthesizerActivity.class.getName();
+    private Button button1;
+    private Button button2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        button1 = (Button)findViewById(R.id.button1);
+        button2 = (Button)findViewById(R.id.button2);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_synthesizer);
     }
