341a5f0fe50bd72f99a532aef9a4a659a57101bd
alcantara94106 <alcantara4106@mydusd.org>
2017-01-23 11:50:35 -0800

    #29 :smirk_cat:
    not working

--- Synthesizer/app/src/main/java/com/example/alcantara94106/synthesizer/SynthesizerActivity.java
+++ Synthesizer/app/src/main/java/com/example/alcantara94106/synthesizer/SynthesizerActivity.java
@@ -1,7 +1,12 @@
 package com.example.alcantara94106.synthesizer;
 
+import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Button;
 
 public class SynthesizerActivity extends AppCompatActivity {
@@ -11,9 +16,16 @@ public class SynthesizerActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
+        setContentView(R.layout.activity_synthesizer);
+
         button1 = (Button)findViewById(R.id.button1);
         button2 = (Button)findViewById(R.id.button2);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_synthesizer);
+    }
+    public void onButton1Click(View thing){
+        Log.i(TAG, "Button 1 Clicked");
+    }
+    public void onButton2Click(View thing){
+        Log.i(TAG, "Button 2 Clicked");
     }
 }
