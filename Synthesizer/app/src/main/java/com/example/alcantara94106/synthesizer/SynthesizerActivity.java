5724104e2c1dd1df386a2f487a24cf50a8f64f73
alcantara94106 <alcantara4106@mydusd.org>
2017-01-24 11:43:56 -0800

    one more

--- Synthesizer/app/src/main/java/com/example/alcantara94106/synthesizer/SynthesizerActivity.java
+++ Synthesizer/app/src/main/java/com/example/alcantara94106/synthesizer/SynthesizerActivity.java
@@ -5,8 +5,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Button;
 
 public class SynthesizerActivity extends AppCompatActivity {
