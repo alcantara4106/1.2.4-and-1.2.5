158e0194d0bfa6a4c61ac6e41274c11c0c054311
alcantara4106 <alcantara4106@mydusd.org>
2017-02-03 11:46:56 -0800

    finished 38... kinda

--- Synthesizer/app/src/main/java/com/example/alcantara94106/synthesizer/SynthesizerActivity.java
+++ Synthesizer/app/src/main/java/com/example/alcantara94106/synthesizer/SynthesizerActivity.java
@@ -9,10 +9,39 @@ import android.widget.Button;
 
 public class SynthesizerActivity extends AppCompatActivity {
     private static final String TAG = SynthesizerActivity.class.getName();
+    private Button mAButton;
+    private Button mBButton;
+    private Button mBBButton;
+    private Button mCButton;
+    private Button mCSButton;
+    private Button mDButton;
+    private Button mDSButton;
     private Button mEButton;
     private Button mFButton;
+    private Button mFSButton;
+    private Button mGButton;
+    private Button mGSButton;
+    private Button mHighEButton;
+    private Button mHighFButton;
+    private Button mHighFSButton;
+    private Button mHighGButton;
+    private MediaPlayer mpA;
+    private MediaPlayer mpB;
+    private MediaPlayer mpBB;
+    private MediaPlayer mpC;
+    private MediaPlayer mpCS;
+    private MediaPlayer mpD;
+    private MediaPlayer mpDS;
     private MediaPlayer mpE;
     private MediaPlayer mpF;
+    private MediaPlayer mpFS;
+    private MediaPlayer mpG;
+    private MediaPlayer mpGS;
+    private MediaPlayer mpHighE;
+    private MediaPlayer mpHighF;
+    private MediaPlayer mpHighFS;
+    private MediaPlayer mpHighG;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -21,8 +50,59 @@ public class SynthesizerActivity extends AppCompatActivity {
 
         mEButton = (Button)findViewById(R.id.eButton);
         mFButton = (Button)findViewById(R.id.fButton);
+
+        mpA = MediaPlayer.create(this, R.raw.scalea);
+        mpB = MediaPlayer.create(this, R.raw.scaleb);
+        mpBB = MediaPlayer.create(this, R.raw.scalebb);
+        mpC = MediaPlayer.create(this, R.raw.scalec);
+        mpCS = MediaPlayer.create(this, R.raw.scalecs);
+        mpD = MediaPlayer.create(this, R.raw.scaled);
+        mpDS = MediaPlayer.create(this, R.raw.scaleds);
         mpE = MediaPlayer.create(this, R.raw.scalee);
         mpF = MediaPlayer.create(this, R.raw.scalef);
+        mpFS = MediaPlayer.create(this, R.raw.scalefs);
+        mpG = MediaPlayer.create(this, R.raw.scaleg);
+        mpGS = MediaPlayer.create(this, R.raw.scalegs);
+        mpHighE = MediaPlayer.create(this, R.raw.scalehighe);
+        mpHighF = MediaPlayer.create(this, R.raw.scalehighf);
+        mpHighFS = MediaPlayer.create(this, R.raw.scalehighfs);
+        mpHighG = MediaPlayer.create(this, R.raw.scalehighg);
+    }
+
+    public void onAButtonClick(View v){
+        mpA.seekTo(0);
+        Log.i(TAG, "Button A Clicked");
+        mpA.start();
+    }
+    public void onBButtonClick(View v){
+        mpB.seekTo(0);
+        Log.i(TAG, "Button B Clicked");
+        mpB.start();
+    }
+    public void onBBButtonClick(View v){
+        mpBB.seekTo(0);
+        Log.i(TAG, "Button BB Clicked");
+        mpBB.start();
+    }
+    public void onCButtonClick(View v){
+        mpC.seekTo(0);
+        Log.i(TAG, "Button C Clicked");
+        mpC.start();
+    }
+    public void onCSButtonClick(View v){
+        mpCS.seekTo(0);
+        Log.i(TAG, "Button CS Clicked");
+        mpCS.start();
+    }
+    public void onDButtonClick(View v){
+        mpD.seekTo(0);
+        Log.i(TAG, "Button D Clicked");
+        mpD.start();
+    }
+    public void onDSButtonClick(View v){
+        mpDS.seekTo(0);
+        Log.i(TAG, "Button DS Clicked");
+        mpDS.start();
     }
     public void onEButtonClick(View v){
         mpE.seekTo(0);
@@ -33,5 +113,40 @@ public class SynthesizerActivity extends AppCompatActivity {
         mpF.seekTo(0);
         Log.i(TAG, "Button F Clicked");
         mpF.start();
+    }
+    public void onFSButtonClick(View v){
+        mpFS.seekTo(0);
+        Log.i(TAG, "Button FS Clicked");
+        mpFS.start();
+    }
+    public void onGButtonClick(View v){
+        mpG.seekTo(0);
+        Log.i(TAG, "Button G Clicked");
+        mpG.start();
+    }
+    public void onGSButtonClick(View v){
+        mpGS.seekTo(0);
+        Log.i(TAG, "Button GS Clicked");
+        mpGS.start();
+    }
+    public void onHighEButtonClick(View v){
+        mpHighE.seekTo(0);
+        Log.i(TAG, "Button High E Clicked");
+        mpHighE.start();
+    }
+    public void onHighFButtonClick(View v){
+        mpHighF.seekTo(0);
+        Log.i(TAG, "Button High F Clicked");
+        mpHighF.start();
+    }
+    public void onHighFSButtonClick(View v){
+        mpHighFS.seekTo(0);
+        Log.i(TAG, "Button High FS Clicked");
+        mpHighFS.start();
+    }
+    public void onHighGButtonClick(View v){
+        mpHighG.seekTo(0);
+        Log.i(TAG, "Button High G Clicked");
+        mpHighG.start();
     }
 }
